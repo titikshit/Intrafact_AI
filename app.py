bad70d79af0d466b6187bf2af67cb6b8230cb13f
titikshit <titikshit@gmail.com>
2026-02-11 10:54:32 +0530

    feat: integrate metadata and vector storage to main pipeline

--- app.py
+++ app.py
@@ -3,10 +3,15 @@ from intrafact.ingestion.file_ingestor import ingestor
 from intrafact.normalization.normalizer import TextNormalizer
 from intrafact.processing.chunker import TextChunker 
 from intrafact.processing.embedder import TextEmbedder 
+from intrafact.storage.metadata_store import MetadataStore
+from intrafact.storage.vector_store import VectorDB
+import uuid
 
 def run_pipeline():
     print("-----Starting Pipeline------")
-
+    
+    meta_store = MetadataStore()
+    vector_db = VectorDB()
 
     #Step 1 Ingestion
     print("-----Starting Ingestion------")
@@ -26,35 +31,50 @@ def run_pipeline():
     chunker = TextChunker()
     embedder = TextEmbedder()
 
+    processed_count = 0
     for file in raw_data:
         original_file_name = file["metadata"]["file_name"]
+        file_hash = file["metadata"]["file_hash"]
+
         print(f"Normalising {original_file_name}")
         # Step 3 normalising
+        if meta_store.document_exists(file_hash):
+            print("File already processed, skipped")
+            continue
+
         try:
+            doc_id = str(uuid.uuid4())
             normalized_data = norm.normalize(
                 file['raw_text'], 
                 file['metadata']
             )
             # Step 4 chunking
             chunks = chunker.process_chunks(normalized_data)
+        
+            
+            for chunk in chunks:
+                chunk["parent_id"] = doc_id
+            
             print(f"   ↳ Split into {len(chunks)} chunks.")
+
             # Step 5 embedding
             chunks_with_vectors = embedder.embed_chunks(chunks)
             print(f"   ↳ Generated {len(chunks_with_vectors[0]['embedding'])}-dimension vectors.")
+            
+            # Step 6 saving vectors
+            vector_db.add_chunks(chunks_with_vectors)
 
-            for i, chunk_obj in enumerate(chunks):
-                save_path = norm.save_object(
-                    chunk_obj, 
-                    original_file_name
-                )
-                print(f"      [Chunk {i+1}] Saved to: {save_path.name}")
+            # Step 7 saving metadata
+            meta_store.register_document(doc_id, original_file_name, file["metadata"])
+
+            processed_count += 1
             
             print("   ✅ Processing Complete!")
 
         except Exception as e:
             print(f"❌ Error processing {original_file_name}: {e}")
 
-    print("-----Pipeline Complete------")
+    print(f"\n----- 🎉 Pipeline Finished. Processed {processed_count} new files. -----")
 
 if __name__== "__main__":
     run_pipeline()
